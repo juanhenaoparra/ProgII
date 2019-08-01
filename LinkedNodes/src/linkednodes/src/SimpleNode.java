6cadb0ae110baec9f76f9f897d989f21d5df2775
JuanHenaoParra <parjuag@gmail.com>
2019-08-01 10:55:12 -0500

    Teoria sobre listas enlazadas y doblemente enlazadas

---
+++ LinkedNodes/src/linkednodes/src/SimpleNode.java
@@ -0,0 +1,18 @@
+package linkednodes.src;
+
+/**
+ * @author parju
+ */
+public class SimpleNode<T> {
+    
+    private T data = null;
+    private SimpleNode<T> nextNode = null;
+    
+    public SimpleNode(T newData){
+        this.data = newData;
+    }
+    
+    //insertAfter, removeafter, get & set, 
+    
+
+}
