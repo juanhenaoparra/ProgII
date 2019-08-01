8a08e0a3f257072d7786e10f919ee9471cef10f3
JuanHenaoParra <parjuag@gmail.com>
2019-08-01 10:55:50 -0500

    Proyecto de iniciacion al manejo de interfaces en JAVA

---
+++ DemoUI/src/demoui/model/Person.java
@@ -0,0 +1,29 @@
+package demoui.model;
+
+/**
+ * @author parju
+ */
+public class Person {
+    private String firstName = "";
+    private String lastName = "";
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+    
+    public String getFullName(){
+        return firstName + " " + lastName;
+    }
+}
